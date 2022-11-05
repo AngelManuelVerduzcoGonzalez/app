127b50cfa6dbe359a8b7e359aed2d0e097926cd7
AngelManuelVerduzcoGonzalez <averduzco17@ucol.mx>
2022-11-04 18:51:12 -0600

    v7

---
+++ index.js
@@ -0,0 +1,20 @@
+let variable = "Hola Mundo";
+
+alert(variable);
+
+console.log(variable);
+
+function saludo() {
+    let nombre= "Angel";
+    let variable = "Hola "+ nombre;
+    alert (variable);
+}
+saludo();
+function SUMA(numero1,numero2) {
+    let resultado=numero1+numero2;
+    alert("El resultado es "+resultado);
+}
+prompt("Hola, esto es un prompt");
+SUMA(1,7);
+SUMA(13,45);
+SUMA(16,27);
