ba6b0631fcab08bfda0662154c0e031527865737
AngelManuelVerduzcoGonzalez <averduzco17@ucol.mx>
2022-11-09 19:01:47 -0600

    version8_JS

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
-let variable = "Hola Mundo";
+// let variable = "Hola Mundo";
 
-alert(variable);
+// alert(variable);
 
-console.log(variable);
+// console.log(variable);
 
-function saludo() {
-    let nombre= "Angel";
-    let variable = "Hola "+ nombre;
-    alert (variable);
+// function saludo() {
+//     let nombre= "Angel";
+//     let variable = "Hola "+ nombre;
+//     alert (variable);
+// }
+// saludo();
+// function SUMA(numero1,numero2) {
+//     let resultado=numero1+numero2;
+//     alert("El resultado es "+resultado);
+// }
+// SUMA(1,7);
+// let numero = parseFloat(prompt("Dame un número"));
+// let numero2 =parseFloat(prompt("Dame otro número"));
+
+// console.log(typeof (numero + numero2));
+
+// document.write(numero + numero2);
+
+let numeros = [3,"Angel ",true,false,"hola "]; 
+
+//los array tienen un indice y comienza en 0
+
+
+//el ciclo for es un bucle
+//el SCOPE es el alcance 
+// for (let i = 0; i < numeros.length; i++) {
+    
+//     console.log(numeros[i]);
+    
+//     document.write(numeros[i]+ "<br>");
+// }
+let variablee = true;
+if (variablee) {
+    document.write(variablee)
 }
-saludo();
-function SUMA(numero1,numero2) {
-    let resultado=numero1+numero2;
-    alert("El resultado es "+resultado);
+
+if (variablee = true) {
+    document.write("Mi variable es true")
+    
+} else {
+    document.write("Mi variable es falsa")
+    
 }
-prompt("Hola, esto es un prompt");
-SUMA(1,7);
-SUMA(13,45);
-SUMA(16,27);
+
+if (condition) {
+    
+} else {
+    if (condition) {
+        
+    } else {
+
+    }
+}
+let dia = "Martes"
+switch (dia) {
+    case "Lunes":
+        console.log("El dia es Lunes");
+        break;
+    case "Martes":
+        console.log("El dia es Martes");
+    default:
+        console.log("Es otro dia de la semana");
+        break;
+}
